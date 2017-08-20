a1a3e7cb6011f26c5b3f8f81f24221eb11b36006
oleksavolodymyr <oleksavolodymyr@gmail.com>
2017-08-20 18:06:22 +0300

    Create rest service

---
+++ src/main/java/com/epam/lab/service/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.epam.lab.service;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET
+    @Path("echo/{input}")
+    public Response ping(@PathParam("input") String input) {
+        return Response.status(200).entity(input).build();
+    }
+
+    @POST
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/jsonBean")
+    public Response modifyJson(JsonBean input) {
+        input.setVal2(input.getVal1());
+        return Response.ok().entity(input).build();
+    }
+}
+
