fa2638cad004ba25f8137c46a2c5e734e7ff2822
oleksavolodymyr <oleksavolodymyr@gmail.com>
2017-08-22 12:54:47 +0300

    Create soap client

---
+++ src/com/epam/lab/service/Copter.java
@@ -0,0 +1,76 @@
+
+package com.epam.lab.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for copter complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="copter"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="distance" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="height" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "copter", propOrder = {
+    "distance",
+    "height"
+})
+public class Copter {
+
+    protected int distance;
+    protected int height;
+
+    /**
+     * Gets the value of the distance property.
+     * 
+     */
+    public int getDistance() {
+        return distance;
+    }
+
+    /**
+     * Sets the value of the distance property.
+     * 
+     */
+    public void setDistance(int value) {
+        this.distance = value;
+    }
+
+    /**
+     * Gets the value of the height property.
+     * 
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Sets the value of the height property.
+     * 
+     */
+    public void setHeight(int value) {
+        this.height = value;
+    }
+
+	@Override
+	public String toString() {
+		return "Copter [distance=" + this.distance + ", height=" + this.height + "]";
+	}
+
+}
